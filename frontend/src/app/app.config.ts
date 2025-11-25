c76c2dc90ddd62f120d358345c2d5f3e684fa2b6
PachitoLauPai <quisperodrigo00@gmail.com>
2025-11-24 21:26:36 -0500

    configuracion del app config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,13 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { httpErrorInterceptor } from '../core/interceptors/http-error.interceptor';
 
-// Para clase interceptor, NO uses withInterceptors
-// En su lugar, usa provideHttpClient sin withInterceptors
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
-    // El interceptor de clase se registra automáticamente si está en providers
+    provideHttpClient(
+      withInterceptors([httpErrorInterceptor])
+    )
   ]
 };
