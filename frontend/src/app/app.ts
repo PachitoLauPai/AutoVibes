5f0addfff61b09ca8c8a43e0a4e8026fc7cea0fb
Jere1515 <jeremyoceano1503@gmail.com>
2025-12-08 01:26:36 -0500

    Actualizar componente raíz de la aplicación

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, Router } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar';
+import { FooterComponent } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   template: `
     <!-- Mostrar navbar solo en rutas públicas (NO en /admin) -->
     <app-navbar *ngIf="!isAdminRoute"></app-navbar>
     <router-outlet></router-outlet>
+    <!-- Mostrar footer solo en rutas públicas (NO en /admin) -->
+    <app-footer *ngIf="!isAdminRoute"></app-footer>
   `,
   styleUrl: './app.css'
 })
