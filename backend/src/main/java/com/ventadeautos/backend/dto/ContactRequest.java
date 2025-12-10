02d1672c0c83f9b735e4641ab7c2cc22f2b5d097
SebadtianR <sebario12345678@gmail.com>
2025-12-09 23:22:38 -0500

    Cambio ContactRequest.java

--- backend/src/main/java/com/ventadeautos/backend/dto/ContactRequest.java
+++ backend/src/main/java/com/ventadeautos/backend/dto/ContactRequest.java
@@ -12,5 +12,5 @@ public class ContactRequest {
     private String mensaje;
     private Long autoId;  // Opcional - si es consulta sobre un auto específico
     private String estado;  // Nuevo: para cambios de estado por admin (PENDIENTE, EN_PROCESO, VENTA_FINALIZADA, CANCELADO)
-    private String tipoTransaccion;  // COMPRA, VENTA, PENDIENTE
+    private String tipoTransaccion;  
 }
